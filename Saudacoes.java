304acea9f1b7e893de94c134877be54fd383fc7b
unknown <rafael.lopes@BRSPOBITAN70449.la.ad.b2w>
2016-08-04 09:08:37 -0300

    Caixa de dialogo para passar parametros

--- Saudacoes.java
+++ Saudacoes.java
@@ -1,7 +1,19 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 class Saudacao {
     
     public static void main(String args[]){
-      System.out.println(args[0]
-      	+"Benvindo ao Curso");
+      System.out.print("Digite o nome do aluno: ");
+
+      Scanner entrada = new Scanner(System.in);
+
+      //Aqui criamos um objeto que ira receber o nome do aluno
+      /*
+       comentario em bloco
+      */
+      //String nome = entrada.nextLine();
+      String nome = JOptionPane.showInputDialog("Digite o nome do aluno");
+      System.out.println(nome
+      	+" Benvindo ao Curso");
     }
 }
